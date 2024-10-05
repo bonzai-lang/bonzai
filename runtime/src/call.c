17cecd73101dfdcbfb8743f167c2beb463270236
Thomas Vergne <contact@thomas-vergne.fr>
2024-10-05 11:28:03 +0200

    feat(vm): added new native callees

--- runtime/src/call.c
+++ runtime/src/call.c
@@ -14,7 +14,7 @@ void op_call(Module *module, Value callee, int32_t argc) {
 
   module->stack->stack_pointer += local_space - argc;
 
-  int32_t new_pc = module->pc + 4;
+  int32_t new_pc = module->pc + 5;
 
   stack_push(module->stack, MAKE_FRAME(module->gc, new_pc, old_sp, module->base_pointer));
 
@@ -29,7 +29,9 @@ void op_native_call(Module *module, Value callee, int32_t argc) {
   char* fun = GET_NATIVE(callee);
 
   Value* args = malloc(sizeof(Value) * argc);
-  for (int i = 0; i < argc; i++) {
+
+  // Pop args in reverse order
+  for (int i = argc - 1; i >= 0; i--) {
     args[i] = stack_pop(module->stack);
   }
 
@@ -39,13 +41,78 @@ void op_native_call(Module *module, Value callee, int32_t argc) {
 
     stack_push(module->stack, MAKE_INTEGER(argc));
   } else if (strcmp(fun, "exit") == 0) {
-    pthread_exit(NULL);
+    printf("EXIT\n");
+    module->is_terminated = true;
+    pthread_exit(0);
   } else if (strcmp(fun, "+") == 0) {
     ASSERT_ARGC("+", argc, 2);
     ASSERT_TYPE("+", args[0], TYPE_INTEGER);
     ASSERT_TYPE("+", args[1], TYPE_INTEGER);
 
     stack_push(module->stack, MAKE_INTEGER(GET_INT(args[0]) + GET_INT(args[1])));
+  } else if (strcmp(fun, "-") == 0) {
+    ASSERT_ARGC("-", argc, 2);
+    ASSERT_TYPE("-", args[0], TYPE_INTEGER);
+    ASSERT_TYPE("-", args[1], TYPE_INTEGER);
+
+    stack_push(module->stack, MAKE_INTEGER(GET_INT(args[0]) - GET_INT(args[1])));
+  } else if (strcmp(fun, "*") == 0) {
+    ASSERT_ARGC("*", argc, 2);
+    ASSERT_TYPE("*", args[0], TYPE_INTEGER);
+    ASSERT_TYPE("*", args[1], TYPE_INTEGER);
+
+    stack_push(module->stack, MAKE_INTEGER(GET_INT(args[0]) * GET_INT(args[1])));
+  } else if (strcmp(fun, "/") == 0) {
+    ASSERT_ARGC("/", argc, 2);
+    ASSERT_TYPE("/", args[0], TYPE_INTEGER);
+    ASSERT_TYPE("/", args[1], TYPE_INTEGER);
+
+    stack_push(module->stack, MAKE_INTEGER(GET_INT(args[0]) / GET_INT(args[1])));
+  } else if (strcmp(fun, "==") == 0) {
+    ASSERT_ARGC("==", argc, 2);
+
+    stack_push(module->stack, MAKE_INTEGER(value_eq(args[0], args[1])));
+  } else if (strcmp(fun, "<") == 0) {
+    ASSERT_ARGC("<", argc, 2);
+    ASSERT_TYPE("<", args[0], TYPE_INTEGER);
+    ASSERT_TYPE("<", args[1], TYPE_INTEGER);
+
+    stack_push(module->stack, MAKE_INTEGER(GET_INT(args[0]) < GET_INT(args[1])));
+  } else if (strcmp(fun, ">") == 0) {
+    ASSERT_ARGC(">", argc, 2);
+    ASSERT_TYPE(">", args[0], TYPE_INTEGER);
+    ASSERT_TYPE(">", args[1], TYPE_INTEGER);
+
+    stack_push(module->stack, MAKE_INTEGER(GET_INT(args[0]) > GET_INT(args[1])));
+  } else if (strcmp(fun, "<=") == 0) {
+    ASSERT_ARGC("<=", argc, 2);
+    ASSERT_TYPE("<=", args[0], TYPE_INTEGER);
+    ASSERT_TYPE("<=", args[1], TYPE_INTEGER);
+
+    stack_push(module->stack, MAKE_INTEGER(GET_INT(args[0]) <= GET_INT(args[1])));
+  } else if (strcmp(fun, ">=") == 0) {
+    ASSERT_ARGC(">=", argc, 2);
+    ASSERT_TYPE(">=", args[0], TYPE_INTEGER);
+    ASSERT_TYPE(">=", args[1], TYPE_INTEGER);
+
+    stack_push(module->stack, MAKE_INTEGER(GET_INT(args[0]) >= GET_INT(args[1])));
+  } else if (strcmp(fun, "&&") == 0) {
+    ASSERT_ARGC("&&", argc, 2);
+    ASSERT_TYPE("&&", args[0], TYPE_INTEGER);
+    ASSERT_TYPE("&&", args[1], TYPE_INTEGER);
+
+    stack_push(module->stack, MAKE_INTEGER(GET_INT(args[0]) && GET_INT(args[1])));
+  } else if (strcmp(fun, "||") == 0) {
+    ASSERT_ARGC("||", argc, 2);
+    ASSERT_TYPE("||", args[0], TYPE_INTEGER);
+    ASSERT_TYPE("||", args[1], TYPE_INTEGER);
+
+    stack_push(module->stack, MAKE_INTEGER(GET_INT(args[0]) || GET_INT(args[1])));
+  } else if (strcmp(fun, "not") == 0) {
+    ASSERT_ARGC("not", argc, 1);
+    ASSERT_TYPE("not", args[0], TYPE_INTEGER);
+
+    stack_push(module->stack, MAKE_INTEGER(!GET_INT(args[0])));
   } else if (strcmp(fun, "value") == 0) {
     ASSERT_ARGC("value", argc, 1);
     ASSERT_TYPE("value", args[0], TYPE_MUTABLE);
@@ -55,7 +122,7 @@ void op_native_call(Module *module, Value callee, int32_t argc) {
     THROW_FMT("Unknown native function %s", fun);
   }
 
-  module->pc += 4;
+  module->pc += 5;
 }
 
 InterpreterFunc interpreter_table[] = { op_native_call, op_call };
