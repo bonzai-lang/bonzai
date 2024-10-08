3cd797377bb0d8600268eee75411d5b08f035de1
Thomas Vergne <contact@thomas-vergne.fr>
2024-10-08 21:23:07 +0200

    test(main): added typechecking tester to main tests

--- test/Main.hs
+++ test/Main.hs
@@ -3,9 +3,11 @@ module Main (main) where
 import Test.Hspec
 import Internal.Parser qualified as Parser
 import Internal.Module qualified as Module
+import Internal.Typechecking qualified as Type
 
 main :: IO ()
 main = do
   hspec $ do
     Parser.testParser
     Module.testModuleImport
+    Type.testTypechecking
