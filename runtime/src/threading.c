a6a7dd829575826d6553798ed19f069de4a37aed
Thomas Vergne <contact@thomas-vergne.fr>
2024-10-09 15:38:17 +0200

    feat(perf): optimized threading module creation

--- runtime/src/threading.c
+++ runtime/src/threading.c
@@ -14,17 +14,7 @@ Value list_get(Module* mod, Value list, uint32_t idx) {
   return l->as_ptr[idx];
 }
 
-Value call_threaded(Module *module, Value callee, int32_t argc, Value *argv) {
-  Module *new_module = malloc(sizeof(Module));
-  new_module->stack = stack_new();
-  new_module->max_objects = INIT_OBJECTS;
-  new_module->num_objects = 0;
-  new_module->first_object = NULL;
-
-  pthread_mutex_lock(&module->module_mutex);
-  memcpy(new_module->stack->values, module->stack->values, GLOBALS_SIZE * sizeof(Value));
-  pthread_mutex_unlock(&module->module_mutex);
-  
+Value call_threaded(Module *new_module, Value callee, int32_t argc, Value *argv) {
   int16_t ipc = (int16_t)(callee & MASK_PAYLOAD_INT);
   int16_t local_space = (int16_t)((callee >> 16) & MASK_PAYLOAD_INT);
   int32_t old_sp = new_module->stack->stack_pointer;
@@ -33,16 +23,30 @@ Value call_threaded(Module *module, Value callee, int32_t argc, Value *argv) {
   for (int i = argc - 1; i >= 0; i--) stack_push(new_module, argv[i]);
   for (int i = 0; i < local_space - argc; i++) stack_push(new_module, MAKE_INTEGER(0));
 
-  int32_t new_pc = module->pc + 5;
+  int32_t new_pc = new_module->pc + 5;
 
   Value frame = MAKE_FRAME(new_module, new_pc, old_sp, new_module->base_pointer);
   stack_push(new_module, frame);
 
   new_module->base_pointer = new_module->stack->stack_pointer - 1;
   new_module->stack->stack_pointer++;
-  new_module->callstack = 1;
 
+  Value ret = run_interpreter(new_module, ipc, true, new_module->callstack - 1);
+
+  return ret;
+}
+
+void *actor_run(void *arg) {
+  Actor *actor = (Actor *)arg;
+  Module* module = actor->mod;
+
+  Module* new_module = malloc(sizeof(Module));
+  new_module->stack = stack_new();
+  new_module->first_object = NULL;
+  new_module->num_objects = 0;
+  new_module->max_objects = INIT_OBJECTS;
   pthread_mutex_lock(&module->module_mutex);
+  memcpy(new_module->stack->values, module->stack->values, GLOBALS_SIZE * sizeof(Value));
   new_module->instr_count = module->instr_count;
   new_module->instrs = module->instrs;
   new_module->constants = module->constants;
@@ -51,22 +55,8 @@ Value call_threaded(Module *module, Value callee, int32_t argc, Value *argv) {
   new_module->handles = module->handles;
   new_module->num_handles = module->num_handles;
   pthread_mutex_unlock(&module->module_mutex);
-  Value ret = run_interpreter(new_module, ipc, true, new_module->callstack - 1);
+  new_module->callstack = 1;
 
-  // // Force sweeping remaining allocated objects
-  force_sweep(new_module);
-
-  // Free the stack and the module
-  free(new_module->stack->values);
-  free(new_module->stack);
-  free(new_module);
-
-  return ret;
-}
-
-void *actor_run(void *arg) {
-  Actor *actor = (Actor *)arg;
-  
   while (1) {
     Message *msg = dequeue(actor->queue);
     
@@ -78,18 +68,29 @@ void *actor_run(void *arg) {
     Value *ons = event.ons;
     Value event_func = ons[id];
 
-    call_threaded(actor->mod, event_func, argc, args);
+    call_threaded(new_module, event_func, argc, args);
 
     free(msg);
     free(args);
 
-    if (actor->mod->is_terminated && actor->queue->head == NULL) {
+    if ((new_module->is_terminated || actor->mod->is_terminated) && actor->queue->head == NULL) {
+      force_sweep(new_module);
+      // Free the stack and the module
+      free(new_module->stack->values);
+      free(new_module->stack);
+      free(new_module);
+
       free(actor->queue);
       free(actor);
       pthread_exit(0);
     }
   }
 
+  force_sweep(new_module);
+  // Free the stack and the module
+  free(new_module->stack->values);
+  free(new_module->stack);
+  free(new_module);
   free(actor->queue);
   free(actor);
   return NULL;
