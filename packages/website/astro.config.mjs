f6a017ec37ec9e975fb2850bf7f5043e0d75051f
Thomas Vergne <contact@thomas-vergne.fr>
2024-11-01 23:06:52 +0100

    build(astro): added custom shiki theme and bonzai syntax highlighting

--- packages/website/astro.config.mjs
+++ packages/website/astro.config.mjs
@@ -8,8 +8,27 @@ import tailwind from '@astrojs/tailwind';
 
 import icon from 'astro-icon';
 
+const data = await fetch('https://raw.githubusercontent.com/thomasvergne/bonzai/refs/heads/main/packages/vscode/syntaxes/bonzai.tmLanguage.json');
+const json = await data.json();
+
+import ShikiTheme from 'shiki/themes/github-dark-default.mjs';
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://example.com',
     integrations: [mdx(), sitemap(), tailwind(), icon()],
+    markdown: {
+      shikiConfig: {
+        theme: {
+          ...ShikiTheme,
+          bg: '#0a0a0a',
+        },
+        langs: [
+          {
+            ...json,
+            aliases: ['bonzai'],
+          }
+        ]
+      }
+    }
 });
