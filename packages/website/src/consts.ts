72cd7f595c72e39523e74747b2715ef39998be9b
Thomas Vergne <contact@thomas-vergne.fr>
2024-11-01 20:12:16 +0100

    feat(blog): added blog template

---
+++ packages/website/src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Bonzai Programming Language';
+export const SITE_DESCRIPTION = 'Bonzai is a programming language based on actor models. It compiles down to a custom bytecode.';
